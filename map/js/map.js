e1cf8d06fc626e72d819b0c3d09d64ee1cedc44d
sm2101 <sidarthmittal5@gmail.com>
2021-02-17 16:31:52 +0530

    Map

--- map/js/map.js
+++ map/js/map.js
@@ -1,1 +1,10 @@
-center=[28.45,77.5843];var mymap=L.map("map").setView(center,11);L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",{maxZoom:18,id:"mapbox.dark",accessToken:"pk.eyJ1IjoiYXJqdW4wMDkiLCJhIjoiY2sxYnc5MnY4MGR0dTNucGdvaG5ua3hsMCJ9.poKC0VmKs2fAPwugNM8uQw"}).addTo(mymap);var marker=L.marker(center).addTo(mymap);marker.bindPopup("<b>BU'Hack</b> 2020 <br>Bennett University, Greater Noida").openPopup();var textWrapper=document.querySelector(".ml7 .letters");textWrapper.innerHTML=textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g,"<span class='letter'>$&</span>"),anime.timeline({loop:!1}).add({targets:".ml7 .letter",translateY:["1.1em",0],translateX:["0.55em",0],translateZ:0,rotateZ:[180,0],duration:1250,easing:"easeOutExpo",delay:function(e,a){return 2009+50*a}});
+var center=[28.45,77.5843];
+var mymap=L.map("map").setView(center,13);
+var marker = L.marker(center).addTo(mymap);
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",{
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom:18,
+    id:"mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:"pk.eyJ1IjoiYnVoYWNrIiwiYSI6ImNrbDliM3NwMTJkdXEycXFqaTR0OWVlajYifQ.n4n7zz_ENYkTPZZwv9S8gQ"}).addTo(mymap);var marker=L.marker(center).addTo(mymap);marker.bindPopup("<b>BU'Hack</b> 2021 <br>Bennett University, Greater Noida").openPopup();var textWrapper=document.querySelector(".ml7 .letters");textWrapper.innerHTML=textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g,"<span class='letter'>$&</span>"),anime.timeline({loop:!1}).add({targets:".ml7 .letter",translateY:["1.1em",0],translateX:["0.55em",0],translateZ:0,rotateZ:[180,0],duration:1250,easing:"easeOutExpo",delay:function(e,a){return 2009+50*a}});
